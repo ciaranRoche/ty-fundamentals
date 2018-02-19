3511886a3a6b57a8d1e1940e3ce95f66fc7b6c67
dimitraz <dimitrazuccarelli@gmail.com>
2018-02-19 22:09:30 +0000

    Adding arrays

---
+++ 6-Arrays/1-arrays.js
@@ -0,0 +1,14 @@
+var numbers = [1, 2, 3, 4, 5];
+var letters = ['a', 'b', 'c', 'd', 'e'];
+var conditions = [true, false, true, false, true];
+
+var shoppingList = [
+    "milk", 
+    "orange juice", 
+    "bread"
+];
+
+console.log(numbers);
+console.log(letters);
+console.log(conditions);
+console.log(shoppingList);
---
+++ 6-Arrays/2-arrays.js
@@ -0,0 +1,14 @@
+var ages = [20, 30];
+var names = ["Bob", "Joe"];
+var cities = ["Waterford", "Dublin"];
+
+// indexing always starts at 0
+console.log(ages[0])
+console.log(ages[1])
+
+console.log(names[0])
+console.log(names[1])
+
+console.log(cities[0])
+console.log(cities[1])
+console.log(cities[2]) // 3rd element in 'cities' array?
