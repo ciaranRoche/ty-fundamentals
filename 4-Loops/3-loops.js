bdf107dac55fc3f2ba0b8db26920f9345c677fa4
dimitraz <dimitrazuccarelli@gmail.com>
2018-02-19 21:51:03 +0000

    Adding loops

---
+++ 4-Loops/3-loops.js
@@ -0,0 +1,12 @@
+// While a certain condition is true, 
+// keep looping
+
+var myAge = 30;
+
+while (myAge < 60) {
+    console.log("Ladidadi " + myAge);
+
+    // statement that somehow alters
+    // the loop condition
+    myAge++;
+}
