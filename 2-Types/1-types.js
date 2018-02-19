c2caf633256ab2f90e739087d1791d206304c411
dimitraz <dimitrazuccarelli@gmail.com>
2018-02-19 21:34:36 +0000

    Adding data types

---
+++ 2-Types/1-types.js
@@ -0,0 +1,14 @@
+var letter = 'a';
+var string = "Slightly more than one letter";
+var age = 60;
+var itsAlwaysSunnyInPhiladelphia = true; 
+
+// show types of each variable
+console.log(typeof letter);
+console.log(typeof string);
+console.log(typeof age);
+console.log(typeof itsAlwaysSunnyInPhiladelphia);
+
+// reassign our string a number
+string = 92;
+console.log(typeof string);
---
+++ 2-Types/2-types.ts
@@ -0,0 +1,12 @@
+var total: number;
+
+// can do this
+total = 50;
+
+for (var i = 0; i < 2; i++) {
+    total += 5;
+    console.log(total);
+}
+
+// can't do this
+// total = "Creative non-fiction";
